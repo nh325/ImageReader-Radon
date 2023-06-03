cd9464abc97976ddc5ca228af94a94b1ce0ca4f2
nh325 <104600815+nh325@users.noreply.github.com>
2023-06-03 18:26:53 -0400

    add radon_windows

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ class ImageReader():
     ImageReader is a simple abstraction on top of skimage and matplotlib that
     performs radon transforms and standard deivation analysis on png/jpeg files.
 
+    To save images, you must create two folders named [images] and [session]
+
     Example usage:
       reader = ImageReader("path/to/image")
       reader.read(x1=0,y1=0,x2=100,y2=100)
@@ -43,8 +45,14 @@ class ImageReader():
 
 
     def _radon_helper(self, steps=180):
-        theta = np.linspace(0., 180., 180, endpoint=False)
+        theta = np.linspace(0., 180., steps, endpoint=False)
         sinogram = radon(self.subrect, theta=theta)
+        norm_sinogram = self._normalize(sinogram)
+        return norm_sinogram
+    
+    def _radon_helper(self, image, steps=180):
+        theta = np.linspace(0., 180., steps, endpoint=False)
+        sinogram = radon(image, theta=theta)
         norm_sinogram = self._normalize(sinogram)
         return norm_sinogram
 
@@ -68,15 +76,34 @@ class ImageReader():
     
 
     def stddev_col(self):
+        [x1, y1], [x2, y2] = self.subrect_pos
         norm_sinogram = self._radon_helper()
         std = np.std(norm_sinogram, axis=0)
+
+        plt.clf()
         plt.plot(std)
         plt.title("Standard Deviation of Columns in Image")
         plt.xlabel("Column")
         plt.ylabel("Standard Deviation")
         plt.show()
-
+        plt.savefig(f"images/{self.image_name}_stddev_({x1},{y1})({x2},{y2})")
     
+
+    def stddev_col(self, image, **kwargs):
+        x1, x2 = kwargs.get("x1"), kwargs.get("x2")
+        y1, y2 = kwargs.get("y1"), kwargs.get("y2")
+
+        norm_sinogram = self._radon_helper(image)
+        std = np.std(norm_sinogram, axis=0)
+        
+        plt.clf()
+        plt.plot(std)
+        plt.title("Standard Deviation of Columns in Image")
+        plt.xlabel("Column")
+        plt.ylabel("Standard Deviation")
+        plt.savefig(f'session/{self.image_name}_stddev_({x1},{y1})({x2},{y2})')
+    
+
     def read(self, steps=180, **kwargs):
         x1, y1 = kwargs.get("x1", 0), kwargs.get("y1", 0)
         x2, y2 = kwargs.get("x2", self.x_max), kwargs.get("y2", self.y_max)
@@ -86,6 +113,32 @@ class ImageReader():
         self.stddev_col(steps=steps)
 
   
+    def radon_windows(self, window_size, step):
+      image = self._image
+
+      for i in range(0, self.x_max - window_size, step):
+          for j in range(0, self.y_max - window_size, step):
+              x1, y1 = i, j
+              x2, y2 = i + window_size, j + window_size
+
+              subrect = image[x1:x2, y1:y2]
+              subrect_radon = self._radon_helper(subrect)
+
+              cv2.imwrite(f"session/{self.image_name}_radon_({x1},{y1})({x2},{y2}).jpg", subrect_radon)
+              cv2.imwrite(f"session/{self.image_name}_cropped_({x1},{y1})({x2},{y2}).jpg", subrect)
+
+              self.stddev_col(subrect_radon, x1=x1,y1=y1,x2=x2,y2=y2)
 
 
-ImageReader("images/AVG_20230130_M228_2L_zoom1pt5.jpg").read(x1=155, y1=130, x2=350, y2=340)
+
+
+reader = ImageReader("images/AVG_20230130_M228_2L_zoom1pt5.jpg")
+# reader.read(x1=155, y1=130, x2=350, y2=340)
+
+# reader.set_subrect
+# reader.radon_transform
+# reader.stddev_col
+
+reader.radon_windows(150, 50)
+
+
